9761875a6c0ba6b6c428685c1a837991dc1f47b0
Krivushkina Daria <daridakr@gmail.com>
2024-01-15 20:33:55 +0100

    Improved PlayerInstaller

--- Assets/Scripts/Installers/PlayerInstaller.cs
+++ Assets/Scripts/Installers/PlayerInstaller.cs
@@ -4,35 +4,46 @@ using Zenject;
 
 public class PlayerInstaller : MonoInstaller
 {
-    [SerializeField] private Player _player;
+    [SerializeField] private Player _prefab;
+
+    private Player _playerInstance;
 
     public override void InstallBindings()
     {
         SpawnAndBindPlayer();
+
+        BindComponents();
         BindSaveData();
+        BindRepository();
     }
 
     private void SpawnAndBindPlayer()
     {
-        Player playerInstance =
-            Container.InstantiatePrefabForComponent<Player>(_player.gameObject, _player.CurrentPoint.position, Quaternion.identity, null);
+        _playerInstance = Container.InstantiatePrefabForComponent<Player>
+            (_prefab.gameObject, _prefab.CurrentPoint.position, Quaternion.identity, null);
 
-        Container.Bind<Player>().FromInstance(playerInstance).AsSingle().NonLazy();
+        Container.Bind<Player>().FromInstance(_playerInstance).AsSingle().NonLazy();
+    }
 
-        Container.Bind<PlayerMovement>().FromInstance(playerInstance.Movement).AsSingle();
-        Container.Bind<PlayerTimerCleaner>().FromInstance(playerInstance.Cleaner).AsSingle();
-        Container.Bind<PlayerTimerBuilder>().FromInstance(playerInstance.Builder).AsSingle();
-        Container.Bind<PlayerWallet>().FromInstance(playerInstance.Wallet).AsSingle();
-        Container.Bind<PlayerBuildingsList>().FromInstance(playerInstance.Buildings).AsSingle();
-        Container.Bind<PlayerVillagersList>().FromInstance(playerInstance.Villagers).AsSingle();
-        Container.Bind<PlayerRegionsList>().FromInstance(playerInstance.Regions).AsSingle();
-
-        Container.BindInterfacesAndSelfTo<PlayerLevel>().FromInstance(playerInstance.Level).AsSingle().NonLazy();
-        Container.BindInterfacesAndSelfTo<PlayerCoins>().FromInstance(playerInstance.Coins).AsSingle();
-        Container.BindInterfacesAndSelfTo<PlayerGems>().FromInstance(playerInstance.Gems).AsSingle();
+    private void BindComponents()
+    {
+        Container.Bind<PlayerMovement>().FromInstance(_playerInstance.Movement).AsSingle();
+        Container.Bind<PlayerTimerCleaner>().FromInstance(_playerInstance.Cleaner).AsSingle();
+        Container.Bind<PlayerTimerBuilder>().FromInstance(_playerInstance.Builder).AsSingle();
+        Container.Bind<PlayerWallet>().FromInstance(_playerInstance.Wallet).AsSingle();
+        Container.Bind<PlayerBuildingsList>().FromInstance(_playerInstance.Buildings).AsSingle();
+        Container.Bind<PlayerVillagersList>().FromInstance(_playerInstance.Villagers).AsSingle();
+        Container.Bind<PlayerRegionsList>().FromInstance(_playerInstance.Regions).AsSingle();
     }
 
     private void BindSaveData()
+    {
+        Container.BindInterfacesAndSelfTo<PlayerLevel>().FromInstance(_playerInstance.Level).AsSingle().NonLazy();
+        Container.BindInterfacesAndSelfTo<PlayerCoins>().FromInstance(_playerInstance.Coins).AsSingle();
+        Container.BindInterfacesAndSelfTo<PlayerGems>().FromInstance(_playerInstance.Gems).AsSingle();
+    }
+
+    private void BindRepository()
     {
         Container.Bind<PlayerPointRepository>().AsSingle();
         Container.BindInterfacesAndSelfTo<PlayerPointSaver>().AsSingle();
