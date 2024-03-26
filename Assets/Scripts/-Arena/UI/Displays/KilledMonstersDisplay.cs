e784e09c4950b3aae09c6d4664235d6acd62c4c1
Krivushkina Daria <daridakr@gmail.com>
2024-03-26 20:55:39 +0100

    Displayed killed monsters count

--- Assets/Scripts/-Arena/UI/Displays/KilledMonstersDisplay.cs
+++ Assets/Scripts/-Arena/UI/Displays/KilledMonstersDisplay.cs
@@ -1,9 +1,19 @@
+using TMPro;
 using UnityEngine;
 
 namespace Arena
 {
     public class KilledMonstersDisplay : MonoBehaviour
     {
+        [SerializeField] private EnemySpawner _enemySpawner;
+        [SerializeField] private TMP_Text _counterTextDisplay;
 
+        private int _counter = 0;
+
+        private void OnEnable() => _enemySpawner.MonsterKillded += UpdatedDisplay;
+        private void Start() => _counterTextDisplay.text = _counter.ToString();
+        private void OnDisable() => _enemySpawner.MonsterKillded -= UpdatedDisplay;
+
+        private void UpdatedDisplay() => _counterTextDisplay.text = (++_counter).ToString();
     }
 }
